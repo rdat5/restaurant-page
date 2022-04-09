22a7831435eafb1e9deed0bb281f518c05edd9c7
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-04-08 21:00:05 -0700

    Begin implementing webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const contElem = document.querySelector('.content');
+
+function component()
+{
+    const element = document.createElement('p');
+
+    element.textContent = "Webpack is working";
+
+    return element;
+}
+
+contElem.appendChild(component());
