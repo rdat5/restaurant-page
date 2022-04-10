313eb3046986bd49a7d867c9aae78fba17d5f88b
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-04-09 18:31:29 -0700

    Implement image loading

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import Jellyfish from './valdemaras-d-kixZ0TUROkc-unsplash.jpg';
 
 const contElem = document.querySelector('.content');
 
@@ -7,6 +8,11 @@ function component()
     const element = document.createElement('p');
 
     element.textContent = "Webpack is working";
+
+    const myJelly = new Image();
+    myJelly.src = Jellyfish;
+
+    element.appendChild(myJelly);
 
     return element;
 }
