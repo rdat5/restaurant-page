0182a417d89410e53113187c0a6c94e94d852f42
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-04-18 21:32:08 -0700

    Modify menu to show Food items from array

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,8 @@
-import FoodPath1 from './amirali-mirhashemian-sc5sTPMrVfk-unsplash.jpg';
+import BurgerPath from './amirali-mirhashemian-sc5sTPMrVfk-unsplash.jpg';
+import HotDogPath from './pexels-polina-tankilevitch-4518656.jpg';
+import PizzaPath from './pexels-sydney-troxell-708587.jpg';
+import PancakePath from './pexels-pixabay-357573.jpg';
+import ShakePath from './clarissa-carbungco-uy9DJw9e_vs-unsplash.jpg';
 
 class Food {
     constructor(name, price, imagepath, desc) {
@@ -40,10 +44,18 @@ function menuSetup(main)
     const menuElem = document.createElement('div');
     menuElem.classList.add('menu-element');
 
-    for (let i = 0; i < 6; i++)
+    const menuItems = [];
+    menuItems.push(new Food("Something Burger", "$11.99", BurgerPath, "Our famous Something Burger."));
+    menuItems.push(new Food("Something Burger Jr.", "$8.99", BurgerPath, "A smaller Something Burger"));
+    menuItems.push(new Food("The Triple Dog", "$9.99", HotDogPath, "Three hotdogs. That's it."));
+    menuItems.push(new Food("The Slice", "$3.50", PizzaPath, "A simple slice of pizza."));
+    menuItems.push(new Food("Panks", "4.99", PancakePath, "A tower of pancakes."));
+    menuItems.push(new Food("Milkshake", "6.99", ShakePath, "A frosty chocolate milkshake."));
+
+    // Show all menu items
+    for (let i = 0; i < menuItems.length; i++)
     {
-        const TestFood = new Food("Food" + (i + 1), "12.99", FoodPath1, "Something");
-        menuElem.appendChild(newCardElement(TestFood));
+        menuElem.appendChild(newCardElement(menuItems[i]));
     }
 
     // Add menu to main element
