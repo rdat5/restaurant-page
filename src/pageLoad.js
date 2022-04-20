cee1569cc14c1f4901acbb1644f252a9e63a0c6f
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-04-19 22:53:19 -0700

    Make current tab more visible

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -5,6 +5,9 @@ import { contactSetup } from './contact';
 
 const contentWrapper = document.querySelector(".content");
 const MainElem = document.createElement('div');
+const homeTab = document.createElement('button');
+const menuTab = document.createElement('button');
+const contactTab = document.createElement('button');
 
 function onNavBarClick(buttonClicked)
 {
@@ -14,14 +17,23 @@ function onNavBarClick(buttonClicked)
         case "home":
             console.log("You clicked home!");
             homeSetup(MainElem);
+            homeTab.style.backgroundColor = "#7794DC";
+            menuTab.style.backgroundColor = "#376EC0";
+            contactTab.style.backgroundColor = "#376EC0";
             break;
         case "menu":
             console.log("You clicked Menu!");
             menuSetup(MainElem);
+            homeTab.style.backgroundColor = "#376EC0";
+            menuTab.style.backgroundColor = "#7794DC";
+            contactTab.style.backgroundColor = "#376EC0";
             break;
         case "contact":
             console.log("You wanted to contact us!");
             contactSetup(MainElem);
+            homeTab.style.backgroundColor = "#376EC0";
+            menuTab.style.backgroundColor = "#376EC0";
+            contactTab.style.backgroundColor = "#7794DC";
             break;
     }
 }
@@ -48,21 +60,21 @@ function initialPageSetup()
     const navElem = document.createElement('div');
     navElem.classList.add('page-nav');
 
-    const homeTab = document.createElement('button');
+    // const homeTab = document.createElement('button');
     homeTab.id = "home-tab";
     homeTab.textContent = "Home";
     homeTab.addEventListener("click", () => {onNavBarClick("home")});
 
     navElem.appendChild(homeTab);
 
-    const menuTab = document.createElement('button');
+    // const menuTab = document.createElement('button');
     menuTab.id = "menu-tab";
     menuTab.textContent = "Menu";
     menuTab.addEventListener("click", () => {onNavBarClick("menu")});
 
     navElem.appendChild(menuTab);
 
-    const contactTab = document.createElement('button');
+    // const contactTab = document.createElement('button');
     contactTab.id = "contact-tab";
     contactTab.textContent = "Contact Us";
     contactTab.addEventListener("click", () => {onNavBarClick("contact")});
@@ -85,6 +97,8 @@ function initialPageSetup()
     // Start at home page
     clearSection(MainElem);
     homeSetup(MainElem);
+    menuTab.style.backgroundColor = "#376EC0";
+    contactTab.style.backgroundColor = "#376EC0";
 }
 
 export {initialPageSetup};
